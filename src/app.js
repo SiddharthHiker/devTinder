162f8a88041c3f6074c54d07039dbf188d7106d2
Siddharth Anand <shaktisid5678@gmail.com>
2024-09-23 15:21:46 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,24 +2,30 @@ const express = require("express");
 
 const app = express()
 
-
-app.get("/user", (req , res) => {
-    res.send({firstName: "Siddharth", lastName: "Anand"});
-});
-
-app.post("/user", (req, res) => {
-    res.send("Data SuccessFully save to the DataBase");
-});
-
-app.delete("/user",(req, res) => {
-    res.send("Data Deleted successfully ");
-});
-
-app.use("/Test",(req,res)=> {
-    res.send("Hello from the server Test!");
-});
-
-
+app.use("/user", (req , res, next) => {
+    console.log("Handling the route  user!!");
+    next();
+},
+(req ,res, next) => {
+    console.log("Handling the route  user 2!!");
+    //res.send("2nd Response");
+    next();
+},
+(req ,res, next) => {
+    console.log("Handling the route  user 3!!");
+    //res.send("2nd Response");
+    next();
+},
+(req ,res, next) => {
+    console.log("Handling the route  user 4!!");
+    //res.send("2nd Response");
+    next();
+},
+(req ,res,next) => {
+    console.log("Handling the route  user 5!!");
+    res.send("5nd Response");
+},
+);
 
 app.listen(3000 , () => {
     console.log("Server is successfully listening on port 3000...");
