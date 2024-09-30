ec1e102a7f4c1e5c65429dd3f42958456753b4fd
Siddharth Anand <shaktisid5678@gmail.com>
2024-09-30 15:53:53 +0530

    Validator

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -17,11 +18,21 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique : true,
         trim: true,
+        validate(value) {
+          if(!validator.isEmail(value)){
+            throw new Error("Invalid email address:" + value);
+          }
+         },
     },
     password: {
         type: String,
         required: true,
+        validator(value) {
+            if(!validator.isStrongPassword(value)){
+            throw new Error("Enter a strong Password:" + value);
+        }
     },
+},
     age: {
         type: Number,
         min: 18,
@@ -37,10 +48,17 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl: {
         type: String,
-        default: "https://www.google.co.in/url?sa=i&url=https%3A%2F%2Fdummyimage.com%2F&psig=AOvVaw27V9R4qr9_fLGJiT2MAeMC&ust=1727719207632000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwjLv7Te3eiIAxVjZmwGHdooCbIQjRx6BAgAEBg"
+        default: "https://www.google.co.in/url?sa=i&url=https%3A%2F%2Fdummyimage.com%2F&psig=AOvVaw27V9R4qr9_fLGJiT2MAeMC&ust=1727719207632000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwjLv7Te3eiIAxVjZmwGHdooCbIQjRx6BAgAEBg",
+        validate(value) {
+            if(!validator.isURL(value)){
+              throw new Error("Invalid photo URL:" + value);
+            }
+           },
     },
+    
     about: {
         type: String,
+        maxLength: 255,
         default: "This is a default about of the user!",
     },
     skills: {
